d400bdfd3bb20a2f8bfc836c0d46a0c09a973175
Firuza <firuza.19.fs@gmail.com>
2024-08-03 16:50:51 +0500

    Восстановление файлов проекта без вложенного репозитория

---
+++ projects/library-system/cmd/app/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"projects/internal/book"
+	"projects/internal/model"
+)
+
+func main() {
+	fmt.Println("Library System")
+
+	books := make([]model.Book, 0)
+
+	newBooks := book.NewBooks(books)
+	service := book.NewService(*newBooks)
+
+	// Иммитируем создание книг
+	b1 := service.CreateBook("The Hobbit", "J.R.R Tolkien")
+	b2 := service.CreateBook("1984", "George Orwell")
+
+	// Иммитируем получение спискм книг
+	listBooks := service.ListBooks()
+	fmt.Println("Books in library:")
+	for _, b := range listBooks {
+		fmt.Printf("ID: %d, Title: %s, Author: %s\n", b.ID, b.Title, b.Author)
+	}
+
+	// Имитируем обновление книги
+	updated := service.EditBook(b1.ID, "The Hobbit: An Unexpected Journey", "J.R.R. Tolkien")
+	if updated {
+		fmt.Printf("Book ID %d updated successfully\n", b1.ID)
+	}
+
+	// Имитируем удаление книги
+	deleted := service.RemoveBook(b2.ID)
+	if deleted {
+		fmt.Printf("Book ID %d deleted successfully\n", b2.ID)
+	}
+
+	// Имитируем получение книги по ID
+	foundBook := service.FindBook(b1.ID)
+	if foundBook != nil {
+		fmt.Printf("Found Book - ID: %d, Title: %s, Author: %s\n", foundBook.ID, foundBook.Title, foundBook.Author)
+	}
+}
---
+++ projects/library-system/internal/book/book.go
@@ -0,0 +1,83 @@
+package book
+
+import (
+	"fmt"
+	"projects/internal/model"
+)
+
+type Books struct {
+	Books []model.Book
+}
+
+func NewBooks(books []model.Book) *Books {
+	return &Books{
+		Books: books,
+	}
+}
+
+var lastID int
+
+func init() {
+	lastID = 0
+}
+
+func (b *Books) AddBook(title, author string) model.Book {
+	lastID++
+	book := model.Book{
+		ID:     lastID,
+		Title:  title,
+		Author: author,
+	}
+
+	b.Books = append(b.Books, book)
+
+	fmt.Printf("Book with tittle %s and author %s is created\n", book.Title, book.Author)
+
+	return book
+}
+
+func (b *Books) GetBooks() []model.Book {
+	return b.Books
+}
+
+func (b *Books) GetBookByID(id int) *model.Book {
+	for _, book := range b.Books {
+		if book.ID == id {
+			fmt.Printf("Found book with id %d: %+v\n", id, book)
+			return &book
+		}
+	}
+	return nil
+}
+
+func (b *Books) GetBooksByAuthor(author string) []model.Book {
+	var booksByAuthor []model.Book
+
+	for _, book := range b.Books {
+		if book.Author == author {
+			booksByAuthor = append(booksByAuthor, book)
+		}
+	}
+	return booksByAuthor
+}
+
+func (b *Books) UpdateBook(id int, title, author string) bool {
+	for i, book := range b.Books {
+		if book.ID == id {
+			b.Books[i].Title = title
+			b.Books[i].Author = author
+			return true
+		}
+	}
+	return false
+}
+
+func (b *Books) DeleteBook(id int) bool {
+	for i, book := range b.Books {
+		if book.ID == id {
+			b.Books = append(b.Books[:i], b.Books[i+1:]...)
+			return true
+		}
+	}
+	return false
+}
