4d1e36998f9511c428ebd99c4459fec5baec67c6
Эргашев Халимджон <abdulhalim1992@gmail.com>
2024-09-17 21:52:05 +0500

    switch to gin for routes

--- library-system/internal/handler/middleware/authenticate.go
+++ library-system/internal/handler/middleware/authenticate.go
@@ -9,18 +9,18 @@ import (
 
 func Authenticate() gin.HandlerFunc {
 	return func(c *gin.Context) {
+
 		// Получаем токен из заголовка
-		authToken, ok := c.Get("Authorization")
-		if !ok {
+		authToken := c.Request.Header.Get("Authorization")
+
+		if authToken == "" {
 			log.Printf("Authenticate - authToken is empty")
 			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "Unauthorized"})
 			return
 		}
 
-		strToken := authToken.(string)
-
 		// Получаем ID пользователя из JWT
-		userID, err := utils.ValidateJWT(strToken)
+		userID, err := utils.ValidateJWT(authToken)
 		if err != nil {
 			log.Printf("Authenticate - utils.ValidateJWT error: %v", err)
 			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "Unauthorized"})
