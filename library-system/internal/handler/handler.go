7ad83b2130481d10af295fa2f92df9cc078b8035
Эргашев Халимджон <abdulhalim1992@gmail.com>
2024-09-22 14:29:18 +0500

    add some updates

--- library-system/internal/handler/handler.go
+++ library-system/internal/handler/handler.go
@@ -43,58 +43,58 @@ func (h *Handler) InitRoutes() {
 	bookGroup := v1.Group("/books")
 	{
 		bookGroup.GET("", h.GetBooks)
-		bookGroup.GET("/{id}", h.GetBookByID)
+		bookGroup.GET("/:id", h.GetBookByID)
 		bookGroup.POST("/add", h.AddBook)
-		bookGroup.DELETE("/delete/{id}", h.DeleteBook)
+		bookGroup.DELETE("/delete/:id", h.DeleteBook)
 		bookGroup.PUT("/update", h.UpdateBook)
-		bookGroup.GET("/author/{id}", h.GetBooksByAuthor)
+		bookGroup.GET("/author/:id", h.GetBooksByAuthor)
 	}
 
 	userGroup := v1.Group("/users")
 	{
 		userGroup.GET("", h.GetUsers)
-		userGroup.GET("/{id}", h.GetUserByID)
-		userGroup.DELETE("/delete/{id}", h.DeleteUser)
+		userGroup.GET("/:id", h.GetUserByID)
+		userGroup.DELETE("/delete/:id", h.DeleteUser)
 		userGroup.PUT("/update", h.UpdateUser)
 	}
 
 	authorGroup := v1.Group("/authors")
 	{
 		authorGroup.GET("", h.GetAuthors)
-		authorGroup.GET("/{id}", h.GetAuthorByID)
+		authorGroup.GET("/:id", h.GetAuthorByID)
 		authorGroup.POST("/add", h.AddAuthor)
-		authorGroup.DELETE("/delete/{id}", h.DeleteAuthor)
+		authorGroup.DELETE("/delete/:id", h.DeleteAuthor)
 		authorGroup.PUT("/update", h.UpdateAuthor)
 	}
 
 	reviewGroup := v1.Group("/reviews")
 	{
 		reviewGroup.GET("", h.GetReviews)
-		reviewGroup.GET("/{id}", h.GetReviewByID)
-		reviewGroup.GET("/user/{id}", h.GetReviewsByUser)
-		reviewGroup.GET("/book/{id}", h.GetReviewsByBook)
+		reviewGroup.GET("/:id", h.GetReviewByID)
+		reviewGroup.GET("/user/:id", h.GetReviewsByUser)
+		reviewGroup.GET("/book/:id", h.GetReviewsByBook)
 		reviewGroup.GET("/filter", h.GetReviewsByFilter)
 		reviewGroup.POST("/add", h.AddReview)
-		reviewGroup.DELETE("/delete/{id}", h.DeleteReview)
+		reviewGroup.DELETE("/delete/:id", h.DeleteReview)
 		reviewGroup.PUT("/update", h.UpdateReview)
 	}
 
 	profileGroup := v1.Group("/profiles")
 	{
 		profileGroup.GET("", h.ListProfiles)
-		profileGroup.GET("/{id}", h.GetProfileByID)
+		profileGroup.GET("/:id", h.GetProfileByID)
 		profileGroup.POST("/add", h.CreateProfile)
-		profileGroup.DELETE("/delete/{id}", h.DeleteProfile)
+		profileGroup.DELETE("/delete/:id", h.DeleteProfile)
 		profileGroup.PUT("/update", h.UpdateProfile)
 	}
 
 	borrowGroup := v1.Group("/borrows")
 	{
 		borrowGroup.GET("", h.GetBorrows)
-		borrowGroup.GET("/{id}", h.GetBorrowByID)
-		borrowGroup.GET("/user/{id}", h.GetBorrowByUser)
-		borrowGroup.GET("/book/{id}", h.GetBorrowByBook)
+		borrowGroup.GET("/:id", h.GetBorrowByID)
+		borrowGroup.GET("/user/:id", h.GetBorrowByUser)
+		borrowGroup.GET("/book/:id", h.GetBorrowByBook)
 		borrowGroup.POST("/add", h.CreateBorrow)
-		borrowGroup.PUT("/return/{id}", h.ReturnBook)
+		borrowGroup.PUT("/return/:id", h.ReturnBook)
 	}
 }
